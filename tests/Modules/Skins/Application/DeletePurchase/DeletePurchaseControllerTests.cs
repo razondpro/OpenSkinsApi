f73061f44f1a3839b77e899ceeb816e18be81e6c
Razon Miah <miahrazon@gmail.com>
2023-10-26 09:36:03 +0200

    Testing delete purchase feature

---
+++ tests/Modules/Skins/Application/DeletePurchase/DeletePurchaseControllerTests.cs
@@ -0,0 +1,74 @@
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Moq;
+using OpenSkinsApi.Infrastructure.Http.Core;
+using OpenSkinsApi.Modules.Skins.Application.DeletePurchase;
+using Xunit;
+
+namespace OpenSkinsApi.Tests.Modules.Skins.Application.DeletePurchase
+{
+    public class DeletePurchaseControllerTests
+    {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly DeletePurchaseController _controller;
+
+        public DeletePurchaseControllerTests()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _controller = new DeletePurchaseController(_mediatorMock.Object);
+        }
+
+        [Fact]
+        public async Task Execute_WhenDeleteOwnedSkinCommandSucceeds_ReturnsNoContent()
+        {
+            // Arrange
+            var request = new DeletePurchaseRequestDto(Guid.NewGuid().ToString(), "test@example.com");
+            _mediatorMock.Setup(x => x.Send(It.IsAny<DeleteOwnedSkinCommand>(), default)).ReturnsAsync(LanguageExt.Unit.Default);
+
+            // Act
+            var result = await _controller.Execute(request);
+
+            // Assert
+            result.Should().BeOfType<Results<NoContent, BadRequest<ApiHttpErrorResponse>, StatusCodeHttpResult>>();
+            result.As<Results<NoContent, BadRequest<ApiHttpErrorResponse>, StatusCodeHttpResult>>()
+                .Result.Should().BeEquivalentTo(TypedResults.NoContent());
+        }
+
+        [Fact]
+        public async Task Execute_WhenSkinNotOwned_ReturnsBadRequestWithSkinNotOwnedError()
+        {
+            // Arrange
+            var request = new DeletePurchaseRequestDto(Guid.NewGuid().ToString(), "test@example.com");
+            _mediatorMock.Setup(x => x.Send(It.IsAny<DeleteOwnedSkinCommand>(), default)).ReturnsAsync(new SkinNotOwnedError());
+
+            // Act
+            var result = await _controller.Execute(request);
+
+            // Assert
+            result.Should().BeOfType<Results<NoContent, BadRequest<ApiHttpErrorResponse>, StatusCodeHttpResult>>();
+            result.As<Results<NoContent, BadRequest<ApiHttpErrorResponse>, StatusCodeHttpResult>>()
+                .Result.Should().BeEquivalentTo(TypedResults.BadRequest(new ApiHttpErrorResponse(
+                    title: "Bad Request",
+                    status: StatusCodes.Status400BadRequest,
+                    errors: new List<ErrorDetail> { new("PurchaseId", "Skin not owned") }
+                )));
+        }
+
+        [Fact]
+        public async Task Execute_WhenDeleteOwnedSkinCommandFails_ReturnsInternalServerError()
+        {
+            // Arrange
+            var request = new DeletePurchaseRequestDto(Guid.NewGuid().ToString(), "test@example.com");
+            _mediatorMock.Setup(x => x.Send(It.IsAny<DeleteOwnedSkinCommand>(), default)).ReturnsAsync(new Exception());
+
+            // Act
+            var result = await _controller.Execute(request);
+
+            // Assert
+            result.Should().BeOfType<Results<NoContent, BadRequest<ApiHttpErrorResponse>, StatusCodeHttpResult>>();
+            result.As<Results<NoContent, BadRequest<ApiHttpErrorResponse>, StatusCodeHttpResult>>()
+                .Result.Should().BeEquivalentTo(TypedResults.StatusCode(StatusCodes.Status500InternalServerError));
+        }
+    }
+}
